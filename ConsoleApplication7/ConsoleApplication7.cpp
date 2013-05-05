da820ecb59acd3f791ba80d35c2e2d1e86dc0b5a
trungnim <nimtritrung@gmail.com>
2013-05-04 18:06:01 -0700

    waht

---
+++ ConsoleApplication7/ConsoleApplication7.cpp
@@ -0,0 +1,15 @@
+// ConsoleApplication7.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+// test for reasonable length
+   if (length < 6 || length > 15)
+   {
+      cout << "Password must be between 6 and 15 characters.\n";
+      continue;
+   }
+}
